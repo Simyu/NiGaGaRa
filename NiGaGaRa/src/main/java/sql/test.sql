c74ceaefc072a7d7259acbe4f7a15a15a532a47c
Sujin Lee <leesuj28@gmail.com>
2018-03-21 14:29:54 +0900

    챗봇 db연동 준비중 예제는 끝

--- NiGaGaRa/src/main/java/sql/test.sql
+++ NiGaGaRa/src/main/java/sql/test.sql
@@ -2,3 +2,5 @@ alter table goods Modify(RECEIVER_ADDR varchar(200));
 alter table goods Modify(sender_Addr varchar(200));
 
 select * from goods where goods_num=42
+
+alter table member add mem_eval varchar(10);
